c8f15b57258f96125beab75ce3c27ddf1cfbb0db
Martin Stephen Cox <admin@martinstephencox.com>
2015-10-06 16:22:34 +0100

    Added rotation animation to 3D cube

--- js/simulation/cube.js
+++ js/simulation/cube.js
@@ -1,45 +1,63 @@
-var width = document.getElementById('simulationContainer').offsetWidth, height = document.getElementById('simulationContainer').offsetHeight - 50; 		//REMOVE "-50" from HEIGHT AFTER REMOVE "Main Simulation goes here text"
+var camera, scene, renderer, cube;
 
-var renderer = new THREE.WebGLRenderer ( { antialias: true});
-renderer.setSize(width, height);
-document.getElementById("simulationContainer").appendChild(renderer.domElement);
+init();
+animate();
 
-var scene = new THREE.Scene;
+function init() {
 
+	var width = document.getElementById('simulationContainer').offsetWidth, height = document.getElementById('simulationContainer').offsetHeight - 50; 		//REMOVE "-50" from HEIGHT AFTER REMOVE "Main Simulation goes here text"
 
+	renderer = new THREE.WebGLRenderer ( { antialias: true});
+	renderer.setSize(width, height);
+	document.getElementById("simulationContainer").appendChild(renderer.domElement);
 
-var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);
-var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-cube.rotation.y = Math.PI * 45 / 180;
-
-scene.add(cube);
+	scene = new THREE.Scene;
 
 
 
-var camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 10000);
+	var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);
+	var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
+	cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-camera.position.y = 160;
-camera.position.z = 400;
+	cube.rotation.y = Math.PI * 45 / 180;
 
-camera.lookAt(cube.position);
-
-scene.add(camera);
-
-
-var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
-var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x262B30, side: THREE.BackSide });
-var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
-
-scene.add(skybox);
-
-
-var pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 300, 200);
-
-scene.add(pointLight);
+	scene.add(cube);
 
 
 
-renderer.render(scene, camera);
+	camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 10000);
+
+	camera.position.y = 160;
+	camera.position.z = 400;
+
+	camera.lookAt(cube.position);
+
+	scene.add(camera);
+
+
+	var skyboxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
+	var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x262B30, side: THREE.BackSide });
+	var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+
+	scene.add(skybox);
+
+
+	var pointLight = new THREE.PointLight(0xffffff);
+	pointLight.position.set(0, 300, 200);
+
+	scene.add(pointLight);
+
+
+
+	
+}
+
+function animate() {
+
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
