eefd9c09e1185fa9c119c585933000027a99f624
Martin Stephen Cox <admin@martinstephencox.com>
2015-10-10 14:00:15 +0100

    Add "Restart Scene" button, code refactoring

--- js/simulation/physics_spheres.js
+++ js/simulation/physics_spheres.js
@@ -6,6 +6,7 @@ debugaxis(100);
 animate();
 
 function init() {
+	//Loads physijs files, creates scene etc.
 
 	Physijs.scripts.worker = '/js/external/physijs_worker.js';
 	Physijs.scripts.ammo = '/js/external/ammo.js';
@@ -32,68 +33,7 @@ function init() {
 
 	scene.setGravity(new THREE.Vector3( 0, 25, 50));
 
-	for (var i=0; i < 50; i++) {
-
-		var object;
-
-		if (Math.random() > 0.5) {
-			var randNum = Math.random();
-			var matColor;
-
-			if (randNum < 0.34) {
-				matColor = 0xD9216A;
-			} else if (randNum > 0.34 && randNum < 0.67) {
-				matColor = 0x18ABDB;
-			} else {
-				matColor = 0x18DB3F;
-			}
-
-			var sphereGeometry = new THREE.SphereGeometry(10, 16, 16);
-
-			var sphereMaterial = Physijs.createMaterial(
-	          new THREE.MeshLambertMaterial({
-	            color: matColor
-	          }),
-		      0.3, // friction
-		      0.9 // restitution
-	        );
-
-			object = new Physijs.SphereMesh(sphereGeometry, sphereMaterial);
-		} else {
-			var randNum = Math.random();
-			var matColor;
-
-			if (randNum < 0.34) {
-				matColor = 0xD9216A;
-			} else if (randNum > 0.34 && randNum < 0.67) {
-				matColor = 0x18ABDB;
-			} else {
-				matColor = 0x18DB3F;
-			}
-
-			var boxGeometry = new THREE.BoxGeometry(20, 20, 20);
-
-			var boxMaterial = Physijs.createMaterial(
-	          new THREE.MeshLambertMaterial({
-	            color: matColor
-	          }),
-		      0.3, // friction
-		      0.9 // restitution
-	        );
-
-			object = new Physijs.BoxMesh(boxGeometry, boxMaterial);
-		}		
-
-		object.position.set((( Math.random() - 0.5 ) * 400), (( Math.random()) * 300), (( Math.random() - 1 ) * 300));
-
-		object.rotation.y = Math.PI * 45 / 180;
-
-		object.castShadow = true;
-
-		scene.add(object);
-
-		objects.push(object);
-	}
+	createObjects(50);
 
 	var planeGeometry_A = new THREE.CubeGeometry(600, 600, 2);
 	var planeGeometry_B = new THREE.CubeGeometry(600, 600, 2);
@@ -161,6 +101,7 @@ function init() {
 }
 
 function animate() {
+	//Animates/simulates scene
 
 	fpsStats.begin();
 
@@ -175,7 +116,85 @@ function animate() {
 	fpsStats.end();
 }
 
+function createObjects(numToCreate) {
+	//Creates numToCreate random physics objects to be used in the simulation
+	for (var i=0; i < numToCreate; i++) {
+
+		var object;
+
+		if (Math.random() > 0.5) {
+			var randNum = Math.random();
+			var matColor;
+
+			if (randNum < 0.34) {
+				matColor = 0xD9216A;
+			} else if (randNum > 0.34 && randNum < 0.67) {
+				matColor = 0x18ABDB;
+			} else {
+				matColor = 0x18DB3F;
+			}
+
+			var sphereGeometry = new THREE.SphereGeometry(10, 16, 16);
+
+			var sphereMaterial = Physijs.createMaterial(
+	          new THREE.MeshLambertMaterial({
+	            color: matColor
+	          }),
+		      0.3, // friction
+		      0.9 // restitution
+	        );
+
+			object = new Physijs.SphereMesh(sphereGeometry, sphereMaterial);
+		} else {
+			var randNum = Math.random();
+			var matColor;
+
+			if (randNum < 0.34) {
+				matColor = 0xD9216A;
+			} else if (randNum > 0.34 && randNum < 0.67) {
+				matColor = 0x18ABDB;
+			} else {
+				matColor = 0x18DB3F;
+			}
+
+			var boxGeometry = new THREE.BoxGeometry(20, 20, 20);
+
+			var boxMaterial = Physijs.createMaterial(
+	          new THREE.MeshLambertMaterial({
+	            color: matColor
+	          }),
+		      0.3, // friction
+		      0.9 // restitution
+	        );
+
+			object = new Physijs.BoxMesh(boxGeometry, boxMaterial);
+		}		
+
+		object.position.set((( Math.random() - 0.5 ) * 400), (( Math.random()) * 300), (( Math.random() - 1 ) * 300));
+
+		object.rotation.y = Math.PI * 45 / 180;
+
+		object.castShadow = true;
+
+		scene.add(object);
+
+		objects.push(object);
+	}
+}
+
+function restartSim() {
+	//Removes all existing physics objects from the scene, then generates new physics objects
+	for (var i=0; i < objects.length; i++) {
+		 if(objects[i] != null) {
+			scene.remove(objects[i]);
+		}
+	}
+	//animate();
+	createObjects(50);
+}
+
 function onWindowResize(){
+	//Dynamically resizes renderer and camera when window is resized
 
 	width = document.getElementById('simulationContainer').offsetWidth;
 	height = document.getElementById('simulationContainer').offsetHeight;
@@ -200,7 +219,7 @@ function debugaxis(axisLength){
     //Create axis (point1, point2, colour)
     function createAxis(p1, p2, color){
             var line, lineGeometry = new THREE.Geometry(),
-            lineMat = new THREE.LineBasicMaterial({color: color, lineWidth: 1});
+            lineMat = new THREE.LineBasicMaterial({color: color});
             lineGeometry.vertices.push(p1, p2);
             line = new THREE.Line(lineGeometry, lineMat);
             scene.add(line);
