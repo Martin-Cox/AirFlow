e07d927b6162823264547e35e2d1ab39e22fd947
Martin Stephen Cox <admin@martinstephencox.com>
2015-11-01 15:29:46 +0000

    Rotating icon on loading screen
    UX guidelines for loading screens:
    http://uxpatterns.org/progress/loading-indicators/

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -9,9 +9,10 @@ app.controller('MainController', ['$scope', function($scope) {
         splashElement.style.opacity = 0;	//Set splash element opacity to 0, triggering CSS transition
 
         //Have to remove the element from the DOM, otherwise it would still be there but be invisible, meaning we can't interact with anything else
+        //Only wait 800ms, 1000ms is the time it takes for opacity transition, but user can see sim before that and may want to interact before opacity has finished
         setTimeout(function() {
         	document.getElementsByClassName("main")[0].removeChild(splashElement);
-        },1000);
+        },800);
     };
 
 }]);
