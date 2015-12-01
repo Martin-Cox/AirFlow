750034b772f5f43e70cc7cfe9c2f4fc4ec207ae4
Martin Stephen Cox <admin@martinstephencox.com>
2015-12-01 14:57:01 +0000

    Updated defaultsService with more methods

--- js/services/defaultsService.js
+++ js/services/defaultsService.js
@@ -11,4 +11,40 @@ app.service('defaultsService', function($http) {
 		        console.log("failure");
 			});
 	}
+	this.getFanDefaults = function() {
+			return $http({
+				method: 'GET',
+				url: '/json/defaultFans.json'
+			}).then (function success(response) {
+				//Success
+				return response.data;
+			}, function error(response) {
+				//TODO: Return error message 
+		        console.log("failure");
+			});
+	}
+	this.getProjectDetailsDefaults = function() {
+			return $http({
+				method: 'GET',
+				url: '/json/defaultProjectDetails.json'
+			}).then (function success(response) {
+				//Success
+				return response.data;
+			}, function error(response) {
+				//TODO: Return error message 
+		        console.log("failure");
+			});
+	}
+	this.getNewFanDefaults = function() {
+			return $http({
+				method: 'GET',
+				url: '/json/defaultNewFanDetails.json'
+			}).then (function success(response) {
+				//Success
+				return response.data;
+			}, function error(response) {
+				//TODO: Return error message 
+		        console.log("failure");
+			});
+	}
 });
