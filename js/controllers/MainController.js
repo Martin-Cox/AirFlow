e4c01d6d4aee37d1bc33e45c66a485e7f602f2d7
Martin Stephen Cox <admin@martinstephencox.com>
2016-02-27 14:26:09 +0000

    Fixed update project modified date on details change

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,7 +2,7 @@ var MainController = function($scope, $http) {
 
     $scope.ajaxComplete = false;
 
-    $scope.projectDetails = null;
+    $scope.projectDetails = new Object();
 
     $scope.defaultProjectDetails = null;
     $scope.defaultCase = null;
@@ -84,11 +84,6 @@ var MainController = function($scope, $http) {
         return formattedDate;
     };
 
-    $scope.$watch('projectDetails', function() {
-        //The project details have been changed, update the modified date
-        $scope.projectDetails.dateModified = $scope.getCurrentDate();
-    });
-
     $scope.$watch('editFan', function() {
         //Angular doesn't like it when we set scope.editFan to null in deleteFan() so we have to set it to an empty array and use an override to disable component settings form
         if ($scope.editFan != null && $scope.editFan.length === 0) {
@@ -98,7 +93,13 @@ var MainController = function($scope, $http) {
         }
     });
 
-    $scope.propertiesChange = function(changedProperty) {
+    $scope.projectDetailsChange = function() {
+        //The project details have been changed, update the modified date
+        $scope.projectDetails.dateModified = $scope.getCurrentDate();
+        console.log("update project mod date");
+    }
+
+    $scope.fanPropertiesChange = function(changedProperty) {
         //Called when the user edits a fan property
         $scope.projectDetails.dateModified = $scope.getCurrentDate();
         $scope.editFan.properties.forceVector = $scope.calculateForceVector($scope.editFan);
