f3dc10fc163be52cec0515eac0884f3f81795089
Martin Stephen Cox <admin@martinstephencox.com>
2016-03-21 14:24:18 +0000

    Update comment

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -43,7 +43,7 @@ var MainController = function($scope, $http) {
             splashElement.style.opacity = 0;	//Set splash element opacity to 0, triggering CSS transition
 
             //Have to remove the element from the DOM, otherwise it would still be there but be invisible, meaning we can't interact with anything else
-            //Only wait 800ms, 1000ms is the time it takes for opacity transition, but user can see sim before that and may want to interact before opacity has finished
+            //Only wait 900ms, 1000ms is the time it takes for opacity transition, but user can see sim before that and may want to interact before opacity has finished
 
             setTimeout(function() {
                 //Show Help and Settings buttons
