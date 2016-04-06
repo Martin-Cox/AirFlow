093b8cb5c0995ad4519b29fca7b85bf7502487d8
Martin Stephen Cox <admin@martinstephencox.com>
2016-04-06 16:15:07 +0100

    Include Angular and Angular-mocks in karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'test/clientTests.js', included: true}
+      {pattern: 'test/clientTests.js', included: true},
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js'
     ],
 
 
