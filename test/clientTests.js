84265ba7c1c1a954a274ae69fe83e1c0de86b6f6
Martin Stephen Cox <admin@martinstephencox.com>
2016-04-07 13:11:10 +0100

    Client side test finally setup correctly
    At long last, client side unit testing can begin

--- test/clientTests.js
+++ test/clientTests.js
@@ -1,34 +1,19 @@
+describe('AirFlow Client Side', function() {
+
 beforeEach(module('AirFlowApp'));
 
-var $MainController;
+	describe('Main Controller', function() {	
 
-describe('AirFlow Client Side', function() {
-	describe('#indexOf()', function() {
-		it('should return -1 when the value is not present', function(){
-			assert.equal(-1, [1,2,3].indexOf(5));
-		 	assert.equal(-1, [1,2,3].indexOf(0));
-		});
-	})
-	describe('equality', function() {
-		it('1 should equal 1', function() {
-			expect(1).to.equal(1);
-		});
-		it('1 + 2 should equal 3', function(){
-			expect(1 + 2).to.equal(3);
-		});
-	})
-	describe('Main Controller', function() {
-		beforeEach(inject(function(_$controller_, _$rootScope_){
-			// The injector unwraps the underscores (_) from around the parameter names when matching
-			$controller = _$controller_;
-			$rootScope = _$rootScope_;
+		var scope, MainController;
+
+		beforeEach(inject(function($controller, $rootScope){
+			scope = $rootScope.$new();
+			MainController = $controller('MainController', {$scope: scope});
 		}));
+
 		describe('Testing scope value', function() {
 			it('unitTestValue should be 554 ', function() {
-				var scope = $rootScope.$new();
-				var myController = $controller('MainController', {$scope: scope});
-				console.log
-			 	expect(myController.unitTestValue).to.equal(554);
+			 	expect(scope.unitTestValue).to.equal(554);
 			});
 		});
 	});
