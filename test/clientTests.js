33f5916589917bc67ce713bd1392ab4bb686a780
Martin Stephen Cox <admin@martinstephencox.com>
2016-04-07 14:57:13 +0100

    defaultsService testing

--- test/clientTests.js
+++ test/clientTests.js
@@ -1,20 +1,14 @@
 describe('AirFlow Client Side', function() {
 
 beforeEach(module('AirFlowApp'));
-
 	describe('Defaults Service', function() {	
 		var http;
 		var defaultsService;
-
-		//beforeEach(module('defaultsService'));
-
 		beforeEach(inject(function($httpBackend, _defaultsService_){
 			http = $httpBackend;
-			//defaultsService = $service('defaultsService', {$http: http});
 			defaultsService = _defaultsService_;
 		}));
-
-		describe('Case Defaults', function() {
+		describe('getCaseDefaults', function() {
 			it('should return JSON data', function() {
 				http.whenGET("/json/defaultCase.json").respond({
 					data: {
@@ -35,22 +29,101 @@ beforeEach(module('AirFlowApp'));
 			    http.flush();
 			});
 		});
+		describe('getFanDefaults', function() {
+			it('should return JSON data', function() {
+				http.whenGET("/json/defaultFans.json").respond({
+					data: {
+						simpleProperty: "simpleValue",
+						complexProperty: {
+							deepProperty1: "deepValue1",
+							deepProperty2: "deepValue2",
+						}
+					}
+			    });
+			    defaultsService.getFanDefaults().then(function(response) {
+			    	expect(response.data).to.exist;
+			     	expect(response.data).to.have.property("simpleProperty").and.equal("simpleValue");
+			     	expect(response.data).to.have.property("complexProperty");
+			     	expect(response.data).to.have.deep.property("complexProperty.deepProperty1").and.equal("deepValue1");
+			     	expect(response.data).to.have.deep.property("complexProperty.deepProperty2").and.equal("deepValue2");
+			    });
+			    http.flush();
+			});
+		});
+		describe('getProjectDetailsDefaults', function() {
+			it('should return JSON data', function() {
+				http.whenGET("/json/defaultProjectDetails.json").respond({
+					data: {
+						simpleProperty: "simpleValue",
+						complexProperty: {
+							deepProperty1: "deepValue1",
+							deepProperty2: "deepValue2",
+						}
+					}
+			    });
+			    defaultsService.getProjectDetailsDefaults().then(function(response) {
+			    	expect(response.data).to.exist;
+			     	expect(response.data).to.have.property("simpleProperty").and.equal("simpleValue");
+			     	expect(response.data).to.have.property("complexProperty");
+			     	expect(response.data).to.have.deep.property("complexProperty.deepProperty1").and.equal("deepValue1");
+			     	expect(response.data).to.have.deep.property("complexProperty.deepProperty2").and.equal("deepValue2");
+			    });
+			    http.flush();
+			});
+		});		
+		describe('getNewFanDefaults', function() {
+			it('should return JSON data', function() {
+				http.whenGET("/json/defaultNewFanDetails.json").respond({
+					data: {
+						simpleProperty: "simpleValue",
+						complexProperty: {
+							deepProperty1: "deepValue1",
+							deepProperty2: "deepValue2",
+						}
+					}
+			    });
+			    defaultsService.getNewFanDefaults().then(function(response) {
+			    	expect(response.data).to.exist;
+			     	expect(response.data).to.have.property("simpleProperty").and.equal("simpleValue");
+			     	expect(response.data).to.have.property("complexProperty");
+			     	expect(response.data).to.have.deep.property("complexProperty.deepProperty1").and.equal("deepValue1");
+			     	expect(response.data).to.have.deep.property("complexProperty.deepProperty2").and.equal("deepValue2");
+			    });
+			    http.flush();
+			});
+		});
+		describe('getStatsAnalysis', function() {
+			it('should return JSON data', function() {
+				http.whenGET("/json/statsAnalysis.json").respond({
+					data: {
+						simpleProperty: "simpleValue",
+						complexProperty: {
+							deepProperty1: "deepValue1",
+							deepProperty2: "deepValue2",
+						}
+					}
+			    });
+			    defaultsService.getStatsAnalysis().then(function(response) {
+			    	expect(response.data).to.exist;
+			     	expect(response.data).to.have.property("simpleProperty").and.equal("simpleValue");
+			     	expect(response.data).to.have.property("complexProperty");
+			     	expect(response.data).to.have.deep.property("complexProperty.deepProperty1").and.equal("deepValue1");
+			     	expect(response.data).to.have.deep.property("complexProperty.deepProperty2").and.equal("deepValue2");
+			    });
+			    http.flush();
+			});
+		});
 	});
-
 	describe('Simulation Directive', function() {	
 
 	});
-
-	describe('Main Controller', function() {	
-
+	describe('Main Controller', function() {
 		var scope, MainController;
-
 		beforeEach(inject(function($controller, $rootScope){
 			scope = $rootScope.$new();
 			MainController = $controller('MainController', {$scope: scope});
 			//scope.newProject();
 		}));
-
 		describe('Testing scope value', function() {
 			it('unitTestValue should be 554 ', function() {
 				//console.log(scope);
