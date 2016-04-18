afb3aee5dabcf8074498678cef2034b317c53718
Martin Stephen Cox <admin@martinstephencox.com>
2016-04-18 15:24:41 +0100

    Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -69,7 +69,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Firefox', 'Chrome'],
+    browsers: ['PhantomJS', 'Firefox'],
 
 
     // Continuous Integration mode
