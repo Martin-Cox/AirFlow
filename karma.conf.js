490b7d6397d319bf88cbfd3fd39f5ab4aa5741bf
Martin Stephen Cox <admin@martinstephencox.com>
2016-04-18 15:48:38 +0100

    Chrome support in Travis

--- karma.conf.js
+++ karma.conf.js
@@ -69,7 +69,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Firefox'],
+    browsers: ['PhantomJS', 'Firefox', 'Chrome'],
 
 
     // Continuous Integration mode
