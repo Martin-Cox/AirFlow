083890b8d21f100d4a49eda28daaefe2806e41c9
Martin Stephen Cox <admin@martinstephencox.com>
2016-04-20 11:55:55 +0100

    Uee Google Closure compiler for minification instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var browserify = require('browserify');
-var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var mochaPhantomJS = require('gulp-mocha-phantomjs');
 var Server = require('karma').Server;
 var rename = require("gulp-rename");
+var closureCompiler = require('google-closure-compiler').gulp();
 
 gulp.task('connect', function () {
     connect.server({
         root: '',
         port: 4000
     })
-})
+});
 
 gulp.task('build', function() {
     // Grabs the app.js file
@@ -24,21 +24,24 @@ gulp.task('build', function() {
         .pipe(source('build.js'))
         //Saves it the js/ directory
         .pipe(gulp.dest('./js/'));
-})
+});
 
-gulp.task('minify', function() {
-    //Minifies an existing build.js file
-    gulp.src('./js/build.js')
-        .pipe(uglify())
-        .pipe(rename({
-          suffix: '.min'
+gulp.task('minify', function () {
+    //Minifies an existing build.js file using Google Closure compiler
+    return gulp.src('./js/build.js')
+        .pipe(closureCompiler({
+            compilation_level: 'SIMPLE',
+            language_out: 'ECMASCRIPT5',
+            warning_level: 'QUIET',
+            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            js_output_file: 'build.min.js'
         }))
         .pipe(gulp.dest('./js/'));
-})
+});
 
 gulp.task('watch', function() {
     gulp.watch(['js/**/*.js', '!js/**/build.js'], { interval: 500 }, ['build'])
-})
+});
 
 gulp.task('test', function() {
     //Creates local server, runs mocha tests, then exits process (therefore killing server)
@@ -53,6 +56,6 @@ gulp.task('test', function() {
             singleRun: true
         }).start()
     });
-})
+});
 
-gulp.task('default', ['connect', 'watch'])
+gulp.task('default', ['connect', 'watch']);
